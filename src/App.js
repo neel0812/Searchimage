2e64fde4ba1415f5db11edd2f31a1e306815086f
neel0812 <54058579+neel0812@users.noreply.github.com>
2020-09-02 13:28:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import React,{useState} from 'react';
-import axios from 'axios';
-import './App.css';
-
+mport React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [photo,setPhoto]=useState("");
-  const [clientId,setClientId]=useState("Ua0CjwRrGebzB9XNVoWTvtTqsCo2Z9qQU-nHlRXqGk4");
-  const [result,setResult]=useState([])
-  function handleChange(event){
-    setPhoto(event.target.value);
+  const initState = { photo: "", result: [] };
+
+  const [state, setstate] = useState(initState);
+
+  const clientId = "Ua0CjwRrGebzB9XNVoWTvtTqsCo2Z9qQU-nHlRXqGk4";
+
+  function handleChange(event) {
+    setstate({ ...state, photo: event.target.value });
   }
 
-  function handleSubmit(event){
-    console.log(photo);
-    const url ="https://api.unsplash.com/photos?page=1&client_id="+clientId+"&query="+photo;
-    axios.get(url).then(response=>{
-      console.log(response);
-      setResult(response.data)
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const url =
+      "https://api.unsplash.com/search/photos?page=1&client_id=" +
+      clientId +
+      "&query=" +
+      state.photo +
+      "/";
+
+    console.log({ url });
+
+    axios.get(url).then((response) => {
+      setstate({ ...state, result: response.data.results });
     });
   }
+
   return (
     <div className="App">
-                <h1>Images Search</h1>
-                <input type="text" name="photo" onChange={handleChange} placeholder="Search for photo..."/>
-                <button type="submit" onClick={handleSubmit}>Search</button>
-                <div>
-                {
-                  result.map(photo=>(
-                  <img src={photo.urls.small}/>
-                  ))
-                }
-                </div>
+      {console.log("rendering")}
+      {console.log({ state })}
+      <h1>Images Search</h1>
+      <input
+        type="text"
+        name="photo"
+        onChange={handleChange}
+        placeholder="Search for photo..."
+      />
+
+      <button type="submit" onClick={handleSubmit}>
+        Search
+      </button>
+
+      {state.result.map((photo) => {
+        return <img src={photo.urls.small} key={photo.id} />;
+      })}
     </div>
   );
 }
